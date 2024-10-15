7568a1faf99f310b876190bd4f74093055748ba4
Ghasan Mahfoud <gusmahfoud10@gmail.com>
2024-10-15 10:57:10 -0400

    Some changes

--- src/components/pages/HomePage/Home.tsx
+++ src/components/pages/HomePage/Home.tsx
@@ -6,14 +6,8 @@ const Home: React.FC = () => {
   return (
     <div className="home">
       <section className="welcome-section">
-        <h1>Welcome to MOKRentals</h1>
-        <p>
-          Rent Anything, Anytime! From golf clubs to houses, MOKRentals connects
-          you with the items you need.
-        </p>
+        
       </section>
-      <h2>Featured Rentals</h2>
-      <RentalDisplay />
     </div>
   );
 };
